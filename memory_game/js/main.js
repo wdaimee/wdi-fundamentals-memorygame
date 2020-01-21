a3769a4a9247d76354c9734757fa438782c7f5e9
Waqas <wdaimee@gmail.com>
2020-01-20 20:06:09 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,23 @@
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!")
+	}
+	else {
+		console.log("Sorry, try again.")
+	}
+}
+
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
